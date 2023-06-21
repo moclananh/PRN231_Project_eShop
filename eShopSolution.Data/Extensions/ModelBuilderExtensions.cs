347bf682fcbbb832c07bd77073b40ab1ac21bb18
Thanh Check Merg <kristino5223@gmail.com>
2023-06-21 19:46:41 +0700

    Initial commit

---
+++ eShopSolution.Data/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,148 @@
+﻿using eShopSolution.Data.Entities;
+using eShopSolution.Data.Enums;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Extensions
+{
+    // mockdata
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppConfig>().HasData(
+               new AppConfig() { Key = "HomeTitle", Value = "This is home page of eShopSolution" },
+               new AppConfig() { Key = "HomeKeyword", Value = "This is keyword of eShopSolution" },
+               new AppConfig() { Key = "HomeDescription", Value = "This is description of eShopSolution" }
+               );
+            modelBuilder.Entity<Language>().HasData(
+                new Language() { Id = "vi", Name = "Tiếng Việt", IsDefault = true },
+                new Language() { Id = "en", Name = "English", IsDefault = false });
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category()
+                {
+                    Id = 1,
+                    IsShowOnHome = true,
+                    ParentId = null,
+                    SortOrder = 1,
+                    Status = Status.Active,
+                },
+                 new Category()
+                 {
+                     Id = 2,
+                     IsShowOnHome = true,
+                     ParentId = null,
+                     SortOrder = 2,
+                     Status = Status.Active
+                 },
+                 new Category()
+                 {
+                     Id = 3,
+                     IsShowOnHome = true,
+                     ParentId = null,
+                     SortOrder = 3,
+                     Status = Status.Active
+                 }
+                 , new Category()
+                {
+                    Id = 4,
+                    IsShowOnHome = true,
+                    ParentId = null,
+                    SortOrder = 4,
+                    Status = Status.Active
+                });
+
+            modelBuilder.Entity<CategoryTranslation>().HasData(
+                  new CategoryTranslation() { Id = 1, CategoryId = 1, Name = "Love Flowers", LanguageId = "en", SeoAlias = "love-flowers", SeoDescription = "The Love Flower, also known by its scientific name Amorisia caritatis, is a mesmerizing botanical wonder that symbolizes the essence of love and affection.", SeoTitle = "Hoa Tinh Yeu" },
+                  new CategoryTranslation() { Id = 2, CategoryId = 2, Name = "Birthday Flowers", LanguageId = "en", SeoAlias = "birthday-flowers", SeoDescription = "The Birthday Flower, known scientifically as Festivus celebratus, is a delightful and vibrant botanical treasure that embodies the joy and celebration of a special day.", SeoTitle = "Hoa Sinh Nhat" },
+                  new CategoryTranslation() { Id = 3, CategoryId = 3, Name = "Congratulation Flowers", LanguageId = "en", SeoAlias = "congratulation-flowers", SeoDescription = "The Congratulation Flower, scientifically known as Florens triumphi, is a magnificent and awe-inspiring bloom that embodies the spirit of celebration, achievement, and triumph.", SeoTitle = "Hoa Chuc Mung" },
+                  new CategoryTranslation() { Id = 4, CategoryId = 4, Name = "Condolence Flowers", LanguageId = "en", SeoAlias = "condolence-flowers", SeoDescription = "Condolence flowers hold a special place in expressing sympathy and offering solace during times of grief and loss. ", SeoTitle = "Hoa chia buon" }
+                    );
+
+            modelBuilder.Entity<Product>().HasData(
+           new Product()
+           {
+               Id = 1,
+               DateCreated = DateTime.Now,
+               OriginalPrice = 100000,
+               Price = 200000,
+               Stock = 0,
+               ViewCount = 0,
+           });
+            modelBuilder.Entity<ProductTranslation>().HasData(
+                 new ProductTranslation()
+                 {
+                     Id = 1,
+                     ProductId = 1,
+                     Name = "Hoa Hong Do",
+                     LanguageId = "vi",
+                     SeoAlias = "hoa-hong-do",
+                     SeoDescription = "Hoa hong do",
+                     SeoTitle = "Hoa hong do",
+                     Details = "Hoa hong do detail",
+                     Description = "Giữa cuộc sống với bao nhiêu khó khăn, căng thẳng và tấp nập, một chút nhẹ nhàng, hồn nhiên của đóa hồng đỏ chắc chắn sẽ mang đến niềm vui khó tả cho một ai đó. Sống chậm lại và yêu thương nhiều hơn, chia sẻ nhiều hơn và quan tâm nhiều hơn, hãy để bó hồng này giúp bạn gửi đi thông điệp đó nhé."
+                 },
+                    new ProductTranslation()
+                    {
+                        Id = 2,
+                        ProductId = 1,
+                        Name = "Sunflower",
+                        LanguageId = "en",
+                        SeoAlias = "sun-flower",
+                        SeoDescription = "sun-flower",
+                        SeoTitle = "sun-flower",
+                        Details = "sun-flower",
+                        Description = "Made from the freshest sunflowers and adorned with branches of white calimero, the flower basket shines brightly and full of life, as lucky as the sun on a summer day. This will be a very suitable gift if you want to make an impression on your partners, colleagues, friends, or the people you love and care about the most."
+                    });
+            modelBuilder.Entity<ProductInCategory>().HasData(
+                new ProductInCategory() { ProductId = 1, CategoryId = 1 }
+                );
+
+            // any guid
+            var roleId = new Guid("8D04DCE2-969A-435D-BBA4-DF3F325983DC");
+            var adminId = new Guid("69BD714F-9576-45BA-B5B7-F00649BE00DE");
+            modelBuilder.Entity<AppRole>().HasData(new AppRole
+            {
+                Id = roleId,
+                Name = "admin",
+                NormalizedName = "admin",
+                Description = "Administrator role"
+            });
+
+            var hasher = new PasswordHasher<AppUser>();
+            modelBuilder.Entity<AppUser>().HasData(new AppUser
+            {
+                Id = adminId,
+                UserName = "admin",
+                NormalizedUserName = "admin",
+                Email = "admin@admin.com",
+                NormalizedEmail = "admin@admin.com",
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null, "admin"),
+                SecurityStamp = string.Empty,
+                FirstName = "Admin",
+                LastName = "minator",
+                Dob = new DateTime(2023, 01, 01)
+            });
+
+            modelBuilder.Entity<IdentityUserRole<Guid>>().HasData(new IdentityUserRole<Guid>
+            {
+                RoleId = roleId,
+                UserId = adminId
+            });
+
+            modelBuilder.Entity<Slide>().HasData(
+              new Slide() { Id = 1, Name = "Second Thumbnail label", Description = "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", SortOrder = 1, Url = "#", Image = "/themes/images/carousel/1.png", Status = Status.Active },
+              new Slide() { Id = 2, Name = "Second Thumbnail label", Description = "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", SortOrder = 2, Url = "#", Image = "/themes/images/carousel/2.png", Status = Status.Active },
+              new Slide() { Id = 3, Name = "Second Thumbnail label", Description = "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", SortOrder = 3, Url = "#", Image = "/themes/images/carousel/3.png", Status = Status.Active },
+              new Slide() { Id = 4, Name = "Second Thumbnail label", Description = "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", SortOrder = 4, Url = "#", Image = "/themes/images/carousel/4.png", Status = Status.Active },
+              new Slide() { Id = 5, Name = "Second Thumbnail label", Description = "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", SortOrder = 5, Url = "#", Image = "/themes/images/carousel/5.png", Status = Status.Active },
+              new Slide() { Id = 6, Name = "Second Thumbnail label", Description = "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", SortOrder = 6, Url = "#", Image = "/themes/images/carousel/6.png", Status = Status.Active }
+              );
+        }
+    }
+}
---
+++ eShopSolution.Data/Migrations/20230621124135_newMigration.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class newMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AppConfigs",
+                columns: new[] { "Key", "Value" },
+                values: new object[,]
+                {
+                    { "HomeTitle", "This is home page of eShopSolution" },
+                    { "HomeKeyword", "This is keyword of eShopSolution" },
+                    { "HomeDescription", "This is description of eShopSolution" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AppRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Description", "Name", "NormalizedName" },
+                values: new object[] { new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"), "09e04c94-8db6-4f41-a4fb-5d417378278f", "Administrator role", "admin", "admin" });
+
+            migrationBuilder.InsertData(
+                table: "AppUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"), new Guid("69bd714f-9576-45ba-b5b7-f00649be00de") });
+
+            migrationBuilder.InsertData(
+                table: "AppUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Dob", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"), 0, "d37c0d06-5ee6-44bf-ad7d-c016427a39a9", new DateTime(2023, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "admin@admin.com", true, "Admin", "minator", false, null, "admin@admin.com", "admin", "AQAAAAEAACcQAAAAEIzhxG2maozhfLA/KBlUEAfpwugXtAsuU9W9j032wTqOAJmBs6AvMYg3m4OYnRngTw==", null, false, "", false, "admin" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "IsShowOnHome", "ParentId", "SortOrder", "Status" },
+                values: new object[,]
+                {
+                    { 3, true, null, 3, 1 },
+                    { 4, true, null, 4, 1 },
+                    { 1, true, null, 1, 1 },
+                    { 2, true, null, 2, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Languages",
+                columns: new[] { "Id", "IsDefault", "Name" },
+                values: new object[,]
+                {
+                    { "vi", true, "Tiếng Việt" },
+                    { "en", false, "English" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "DateCreated", "IsFeatured", "OriginalPrice", "Price" },
+                values: new object[] { 1, new DateTime(2023, 6, 21, 19, 41, 34, 768, DateTimeKind.Local).AddTicks(8818), null, 100000m, 200000m });
+
+            migrationBuilder.InsertData(
+                table: "Slides",
+                columns: new[] { "Id", "Description", "Image", "Name", "SortOrder", "Status", "Url" },
+                values: new object[,]
+                {
+                    { 5, "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", "/themes/images/carousel/5.png", "Second Thumbnail label", 5, 1, "#" },
+                    { 1, "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", "/themes/images/carousel/1.png", "Second Thumbnail label", 1, 1, "#" },
+                    { 2, "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", "/themes/images/carousel/2.png", "Second Thumbnail label", 2, 1, "#" },
+                    { 3, "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", "/themes/images/carousel/3.png", "Second Thumbnail label", 3, 1, "#" },
+                    { 4, "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", "/themes/images/carousel/4.png", "Second Thumbnail label", 4, 1, "#" },
+                    { 6, "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.", "/themes/images/carousel/6.png", "Second Thumbnail label", 6, 1, "#" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "CategoryTranslations",
+                columns: new[] { "Id", "CategoryId", "LanguageId", "Name", "SeoAlias", "SeoDescription", "SeoTitle" },
+                values: new object[,]
+                {
+                    { 1, 1, "en", "Love Flowers", "love-flowers", "The Love Flower, also known by its scientific name Amorisia caritatis, is a mesmerizing botanical wonder that symbolizes the essence of love and affection.", "Hoa Tinh Yeu" },
+                    { 2, 2, "en", "Birthday Flowers", "birthday-flowers", "The Birthday Flower, known scientifically as Festivus celebratus, is a delightful and vibrant botanical treasure that embodies the joy and celebration of a special day.", "Hoa Sinh Nhat" },
+                    { 3, 3, "en", "Congratulation Flowers", "congratulation-flowers", "The Congratulation Flower, scientifically known as Florens triumphi, is a magnificent and awe-inspiring bloom that embodies the spirit of celebration, achievement, and triumph.", "Hoa Chuc Mung" },
+                    { 4, 4, "en", "Condolence Flowers", "condolence-flowers", "Condolence flowers hold a special place in expressing sympathy and offering solace during times of grief and loss. ", "Hoa chia buon" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductInCategories",
+                columns: new[] { "CategoryId", "ProductId" },
+                values: new object[] { 1, 1 });
+
+            migrationBuilder.InsertData(
+                table: "ProductTranslations",
+                columns: new[] { "Id", "Description", "Details", "LanguageId", "Name", "ProductId", "SeoAlias", "SeoDescription", "SeoTitle" },
+                values: new object[,]
+                {
+                    { 1, "Giữa cuộc sống với bao nhiêu khó khăn, căng thẳng và tấp nập, một chút nhẹ nhàng, hồn nhiên của đóa hồng đỏ chắc chắn sẽ mang đến niềm vui khó tả cho một ai đó. Sống chậm lại và yêu thương nhiều hơn, chia sẻ nhiều hơn và quan tâm nhiều hơn, hãy để bó hồng này giúp bạn gửi đi thông điệp đó nhé.", "Hoa hong do detail", "vi", "Hoa Hong Do", 1, "hoa-hong-do", "Hoa hong do", "Hoa hong do" },
+                    { 2, "Made from the freshest sunflowers and adorned with branches of white calimero, the flower basket shines brightly and full of life, as lucky as the sun on a summer day. This will be a very suitable gift if you want to make an impression on your partners, colleagues, friends, or the people you love and care about the most.", "sun-flower", "en", "Sunflower", 1, "sun-flower", "sun-flower", "sun-flower" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeDescription");
+
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeKeyword");
+
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeTitle");
+
+            migrationBuilder.DeleteData(
+                table: "AppRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"));
+
+            migrationBuilder.DeleteData(
+                table: "AppUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"), new Guid("69bd714f-9576-45ba-b5b7-f00649be00de") });
+
+            migrationBuilder.DeleteData(
+                table: "AppUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"));
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "ProductInCategories",
+                keyColumns: new[] { "CategoryId", "ProductId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductTranslations",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ProductTranslations",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Slides",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Slides",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Slides",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Slides",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Slides",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Slides",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: "en");
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: "vi");
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
