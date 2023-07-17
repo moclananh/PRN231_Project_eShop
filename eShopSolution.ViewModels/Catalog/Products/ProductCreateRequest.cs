4f49293177f2f05bdcb888d9d0254df16e16b37b
MinhHien <leminhhien.bl@gmail.com>
2023-07-17 19:47:36 +0700

    HienLM

--- eShopSolution.ViewModels/Catalog/Products/ProductCreateRequest.cs
+++ eShopSolution.ViewModels/Catalog/Products/ProductCreateRequest.cs
@@ -23,7 +23,7 @@ namespace eShopSolution.ViewModels.Catalog.Products
         public string SeoAlias { get; set; }
         public string LanguageId { set; get; }
 
-        public bool? IsFeatured { get; set; }
+        public bool IsFeatured { get; set; }
 
         public IFormFile ThumbnailImage { get; set; }
     }
